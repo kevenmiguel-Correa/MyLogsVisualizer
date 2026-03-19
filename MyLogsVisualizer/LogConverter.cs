ca9bdc4c61c5bdaf777ccfdda7f948ac98955761
KCorrea091 <KCorrea091@outlook.com>
2026-03-18 20:48:52 -0400

    Add project files.

---
+++ MyLogsVisualizer/LogConverter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyLogsVisualizer
+{
+    public class LogConverter
+    {
+        private static readonly Regex regex = new Regex(
+           @"\[(.*?) (.*?)\] (.*)",
+           RegexOptions.Compiled);
+
+        public static List<LogDto> Parse(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            var result = new List<LogDto>();
+
+            foreach (var line in lines)
+            {
+                var match = regex.Match(line);
+                var rawLevel = match.Groups[2].Value;
+
+                // "00:00:24 WRN"
+                var parts = rawLevel.Split(' ');
+                var levelShort = parts.Last(); // WRN
+                var time = parts.First();      // 00:00:24
+
+                if (match.Success)
+                {
+                    result.Add(new LogDto
+                    {
+                        Timestamp = match.Groups[1].Value,
+                        Level = ConvertLevel(levelShort),
+                        Message = match.Groups[3].Value,
+                        RawLine = line
+                    });
+                }
+                else
+                {
+                    // Si no matchea, lo agregamos crudo
+                    result.Add(new LogDto
+                    {
+                        Timestamp = "",
+                        Message = line,
+                        Level = "Unknown",
+                        RawLine = line
+                    });
+                }
+            }
+
+            return result;
+        }
+
+        private static string ConvertLevel(string shortLevel)
+        {
+            return shortLevel switch
+            {
+                "INF" => "Information",
+                "WRN" => "Warning",
+                "ERR" => "Error",   
+                "DBG" => "Debug",
+                _ => shortLevel
+            };
+        }
+    }
+}
---
+++ MyLogsVisualizer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyLogsVisualizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<LogDto> AllLogs = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadLog_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Archivos log (*.log;*.txt)|*.log;*.txt|Todos (*.*)|*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                AllLogs = LogConverter.Parse(dialog.FileName);
+                LogGrid.ItemsSource = AllLogs;
+            }
+        }
+
+        private void SearchBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var text = SearchBox.Text.ToLower();
+
+            LogGrid.ItemsSource = AllLogs
+                .Where(x =>
+                        (x.Message ?? "").ToLower().Contains(text) ||
+                        (x.Level ?? "").ToLower().Contains(text) ||
+                        (x.Timestamp ?? "").ToLower().Contains(text))
+                .ToList();
+        }
+    }
+}
